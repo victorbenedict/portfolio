f2df912df5c5a07a8691eeddab7d317543eb2f57
Nasuri <victorbenedict.bulaong@gmail.com>
2024-02-17 16:42:25 +0800

    Modify as experimental

--- next.config.js
+++ next.config.js
@@ -1,16 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   reactStrictMode: true,
+//   basePath: "/github-pages",
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "avatars.githubusercontent.com",
+//       },
+//     ],
+//   },
+// };
+
+// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-  basePath: "/github-pages",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-    ],
+const config = {
+  experimental: {
+    serverActions: true,
   },
 };
 
-module.exports = nextConfig;
+module.exports = config;
