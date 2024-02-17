3ad2649e45e53c610f614566730b3dd0254297b1
Nasuri <victorbenedict.bulaong@gmail.com>
2024-02-17 16:44:52 +0800

    Modify output

--- next.config.js
+++ next.config.js
@@ -1,24 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-//   reactStrictMode: true,
-//   basePath: "/github-pages",
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "avatars.githubusercontent.com",
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
-const config = {
-  experimental: {
-    serverActions: true,
-  },
+const nextConfig = {
+  output: "export",
 };
-
-module.exports = config;
+/* module.exports = nextConfig*/
+export default nextConfig;
