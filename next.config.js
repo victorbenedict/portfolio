3a7448c871da10fcc6daf332f1be0f3b39c9c06b
Nasuri <victorbenedict.bulaong@gmail.com>
2024-02-17 16:50:43 +0800

    Remove output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
-/* module.exports = nextConfig*/
-export default nextConfig;
+
+module.exports = nextConfig;
