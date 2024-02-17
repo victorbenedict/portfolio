756331047010b0535ceba6fe8f24a090fcb24b8b
Nasuri <victorbenedict.bulaong@gmail.com>
2024-02-17 14:56:25 +0800

    Modify nextConfig

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
-
-  // Add basePath
   basePath: "/github-pages",
   images: {
     remotePatterns: [
